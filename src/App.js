769f51d2f3d92a351d135db738687c4f6e9ec700
SAVEEN-J <saveenj1996@gmail.com>
2024-02-02 15:50:24 +0530

    filter changes

--- src/App.js
+++ src/App.js
@@ -21,11 +21,13 @@ function App(props) {
   const[newnotescontent,setnewNotesContent] = useState('');
   //importance
   const[newnotesimportance,setnewnotesImportance] = useState('');
+//read data filter
+  const[showStatus,setShowStatus]=useState('all')
 
 //get data from backend
 useEffect(()=>{
   setNotes(props.notes);
-});
+},[]);
 
 //create referencr
 const newNoteRef =useRef(null);
@@ -54,25 +56,77 @@ const newNoteRef =useRef(null);
   let handleChangeImportant=(event)=>{
     setnewnotesImportance(event.target.value);
 
-
   }
-
+  let handleStatuChange = (event) => {
+      
+    setShowStatus(event.target.value);
+ 
+    //  console.log(event.target.value);
+  }
+  let filterNotes=(notes,status)=>{
+    switch (status) {
+      case 'all':
+       return notes;
+      case 'imp':
+      return notes.filter(note =>note.important===true);
+      case 'nonimp':
+        return notes.filter(note =>note.important===false);
+        case 'on':
+          return notes.filter(note =>note.selectbutton==="on");
+    }
+    
+      }
+     const notesFilter=filterNotes(notes,showStatus)
   return (
     <div>
  
- <h2>CURD OPERATION</h2>
+ <h2 style={{backgroundColor: "lightblue"}}>CURD OPERATION</h2>
 
  <h3>Read Notes</h3>
- <ul>
+ <h2 >Notes filter Option</h2>
 
- {notes.map(note => 
+    <label>
+    <input type='radio' name='filter' value="all" 
+    checked={showStatus ==='all'} 
+    onChange={handleStatuChange}/> All notes
+    </label>
+
+    <label>
+          <input type='radio' name='filter' value="imp" 
+    checked={showStatus ==='imp'} 
+    onChange={handleStatuChange}/>Important notes
+    </label>
+
+    <label>
+    <input type='radio' name='filter' value="nonimp" 
+    checked={showStatus ==='nonimp'} 
+    onChange={handleStatuChange}/>Non important notes
+    </label>
+
+    <label>
+    <input type='radio' name='filter' value="on"
+     checked={showStatus ==='on'} 
+     onChange={handleStatuChange}/>current notes in ON
+    </label>
+
+
+ <ul>
+ {notesFilter.map((note=>
+     
+     <Note key ={note.id} note={note} />
+
+     ))}
+ {/* {notes.map(note => 
 //  console.log(note.id)
    <Note key ={note.id} note={note} />
-  )}
+  )} */}
  </ul>
 
  <h2>Create Notes</h2>
+
+
 <form onSubmit={addNote}>
+
 <label for="fnots">Notes content:  </label>
   <input type="text" id="fnots" name="fnots"
    value={newnotescontent}
@@ -81,10 +135,20 @@ const newNoteRef =useRef(null);
     /><br/><br/>
 
 
-  <label for="fimportant">Notes Important:  </label>
+  {/* <label for="fimportant">Notes Important:  </label>
   <input type="text" id="fimportant" name="fimportant" 
   value={newnotesimportance}
-   onChange={handleChangeImportant}/><br/>
+   onChange={handleChangeImportant}/>
+   <br/> */}
+   <label for="Onoff"> Select Important : </label>
+   <select id="Onoff" onChange={handleChangeImportant} value={newnotesimportance} >
+    <option>Select</option>
+    <option>True</option>
+    <option>False</option>
+
+   </select><br/><br/>
+
+
   {/* <input type="text" id="fimportant" name="fimportant" value={newnotesimportance} onChange={(e)=>setnewnotesImportance(e.target.value)}/><br/> */}
  
  
